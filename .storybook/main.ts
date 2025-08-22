2a3e6d38ff549fd9cb4e5483b6bc9d9b574c0483
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-08-22 19:05:04 +0300

    chore: remove visual tests addon from storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,10 +3,9 @@ import type { StorybookConfig } from "@storybook/nextjs-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest",
+    "@storybook/addon-vitest"
   ],
   framework: {
     name: "@storybook/nextjs-vite",
