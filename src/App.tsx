668c2c104166c71a3d91e81d059a52caf79ab106
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2026-02-14 15:32:14 +0200

    Move providers directory to src directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Providers } from './components/Providers';
+import { Providers } from './providers';
 import { AppShell } from './components/AppShell';
 import { BudgetScreen } from './components/BudgetScreen';
 import { DataManagementScreen } from './components/DataManagementScreen';
