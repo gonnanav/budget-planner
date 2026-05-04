a2540a54413130ea01529a712ddec7691f48cad3
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2026-05-04 19:06:07 +0300

    Replace vite-tsconfig-paths with native Vite tsconfigPaths

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vitest/config'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-    tsconfigPaths(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
     exclude: ['**/node_modules/**', 'tests/**'],
   },
