126c2afdc6a310639c0b2fcb3bd471f382c32434
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2026-05-04 16:31:44 +0300

    Remove Storybook

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,9 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import storybook from "eslint-plugin-storybook";
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([globalIgnores(['dist', 'storybook-static']), {
+export default defineConfig([globalIgnores(['dist']), {
   files: ['**/*.{ts,tsx}'],
   extends: [
     js.configs.recommended,
@@ -18,4 +17,4 @@ export default defineConfig([globalIgnores(['dist', 'storybook-static']), {
     ecmaVersion: 2020,
     globals: globals.browser,
   },
-}, ...storybook.configs["flat/recommended"]])
+}])
