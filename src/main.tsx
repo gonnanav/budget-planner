fe22c1514a8bf0d39b79ee2c9582b281c0f2b39c
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2026-05-16 21:51:55 +0300

    Move infrastructure providers from App to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import './base.css'
 import './theme.css'
+import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { MantineProvider } from '@mantine/core'
+import { Notifications } from '@mantine/notifications'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <MantineProvider>
+        <Notifications />
+        <App />
+      </MantineProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
